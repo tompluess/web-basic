d1d6cd0bcea2d945358fe3db00f0cd9c02dba019
Tom Plüss <tom@skyr.ch>
2016-04-22 14:26:55 +0200

    Initial project structure and Hello World main class.

---
+++ src/main/java/ch/skyr/web/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package ch.skyr.web.basic;
+
+/**
+ * Created by tom on 22.04.16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
